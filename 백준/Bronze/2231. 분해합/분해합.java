76241b00fd44b8e815d4ec2c0da80c802108f369
JeongSeok <ljs0429777@gmail.com>
2024-10-01 19:41:53 +0900

    [Bronze II] Title: 분해합, Time: 116 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,28 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	static int N, result;
 
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
 
-		int n = Integer.parseInt(br.readLine());
-		
-		int result = 0;
-
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < N; i++) {
 			int sum = 0;
 			int number = i;
 
-
 			while (number != 0) {
 				sum += number % 10;
-				number /= 10;
+				number = number / 10;
 			}
 
-			if (sum + i == n) {
+			if (sum + i == N) {
 				result = i;
 				break;
 			}
