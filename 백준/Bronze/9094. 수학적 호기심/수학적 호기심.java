ca81124b1b84c0f48af5037918225b500d46c5ef
JeongSeok <ljs0429777@gmail.com>
2024-11-18 19:09:08 +0900

    [Bronze III] Title: 수학적 호기심, Time: 428 ms, Memory: 24732 KB -BaekjoonHub

--- 백준/Bronze/9094. 수학적 호기심/수학적 호기심.java
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.java
@@ -1,28 +1,31 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
+	static int T, n, m;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int input = Integer.parseInt(br.readLine());
+		T = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < input; i++) {
+		for (int i = 0; i < T; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
-			int result = 0;
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			int count = 0;
 
 			for (int a = 1; a < n; a++) {
 				for (int b = a + 1; b < n; b++) {
-					if(((a * a) + (b * b) + m) % (a * b) == 0) {
-						result++;
+					if (((a * a) + (b * b) + m) % (a * b) == 0) {
+						count++;
 					}
 				}
 			}
-			System.out.println(result);
-		}
 
+			System.out.println(count);
+		}
 	}
 
 }
