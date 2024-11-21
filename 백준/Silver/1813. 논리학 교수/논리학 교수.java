bcfdbed4d60580d51eb03168b813c53f7d315baa
JeongSeok <ljs0429777@gmail.com>
2024-11-22 08:50:51 +0900

    [Silver V] Title: 논리학 교수, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/1813. 논리학 교수/논리학 교수.java
+++ 백준/Silver/1813. 논리학 교수/논리학 교수.java
@@ -1,21 +1,31 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
+	static int N, max;
+	static int[] arr;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		max = -1;
 
-		int caseCount = Integer.parseInt(br.readLine());
-		int[] bool = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		int max = -1;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		for (int i = 0; i <= caseCount; i++) {
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int i = 0; i <= N; i++) {
 			int temp = i;
-			int cnt = (int) Arrays.stream(bool).filter(num -> num == temp).count();
+			int count = (int) Arrays.stream(arr).filter(num -> num == temp).count();
 
-			if (temp == cnt) {
-				max = Math.max(max, cnt);
+			if (temp == count) {
+				max = Math.max(max, count);
 			}
 		}
 
