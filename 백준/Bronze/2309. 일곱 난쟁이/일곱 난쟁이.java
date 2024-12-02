f9728e9671dc13bae242deaa2a238575f19e6dae
JeongSeok <ljs0429777@gmail.com>
2024-12-02 11:32:59 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -4,47 +4,36 @@ import java.util.Arrays;
 
 public class Main {
 
-	static int[] arr, temp;
-	static boolean[] visited;
+	static int[] arr;
+	static int sum, none1, none2;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		arr = new int[9];
-		temp = new int[7];
-		visited = new boolean[9];
 
 		for (int i = 0; i < 9; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
 		}
 
-		dfs(0);
-	}
+		Arrays.sort(arr);
 
-	private static void dfs(int depth) {
-		if (depth == 7) {
-			if (calculate()) {
-				Arrays.stream(temp).sorted().forEach(System.out::println);
-				System.exit(0);
-			}
-		} else {
-			for (int i = 0; i < arr.length; i++) {
-				if (!visited[i]) {
-					visited[i] = true;
-					temp[depth]	= arr[i];
-					dfs(depth + 1);
-					visited[i] = false;
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (sum - arr[i] - arr[j] == 100) {
+					none1 = arr[i];
+					none2 = arr[j];
 				}
 			}
 		}
-	}
 
-	private static boolean calculate() {
-		int sum = 0;
-		for (int i = 0; i < temp.length; i++) {
-			sum += temp[i];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == none1 || arr[i] == none2) {
+				continue;
+			}
+			System.out.println(arr[i]);
 		}
-		return sum == 100;
 	}
 
 }
