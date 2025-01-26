336875f3fb6b3514af00161c376d38d91bb20600
JeongSeok <ljs0429777@gmail.com>
2025-01-26 17:05:58 +0900

    [Silver III] Title: 숫자 야구, Time: 112 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -1,15 +1,15 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-class BallCount {
-
+class BaseBall {
 	int number;
 	int strike;
 	int ball;
 
-	public BallCount(int number, int strike, int ball) {
+	public BaseBall(int number, int strike, int ball) {
 		this.number = number;
 		this.strike = strike;
 		this.ball = ball;
@@ -18,71 +18,59 @@ class BallCount {
 
 public class Main {
 
-	static ArrayList<BallCount> list = new ArrayList<>();
-	static int N, count;
+	static int N, answer;
+	static ArrayList<BaseBall> baseBalls = new ArrayList<>();
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
 			int number = Integer.parseInt(st.nextToken());
 			int strike = Integer.parseInt(st.nextToken());
 			int ball = Integer.parseInt(st.nextToken());
 
-			list.add(new BallCount(number, strike, ball));
+			baseBalls.add(new BaseBall(number, strike, ball));
 		}
 
-		for (int i = 123; i < 988; i++) {
-			int a2 = i / 100;
-			int b2 = i / 10 - (a2 * 10);
-			int c2 = i % 10;
+		for (int i = 123; i < 999; i++) {
+			int count = 0;
 
-			if (a2 == 0 || b2 == 0 || c2 == 0 || a2 == b2 || a2 == c2 || b2 == c2) {
-				continue;
+			int a1 = i / 100;
+			int b1 = (i % 100) / 10;
+			int c1 = i % 10;
+
+			if (a1 == 0 || b1 == 0 || c1 == 0 || a1 == b1 || a1 == c1 || b1 == c1) continue;
+
+			for (int j = 0; j < baseBalls.size(); j++) {
+				BaseBall current = baseBalls.get(j);
+				int strikeCount = 0;
+				int ballCount = 0;
+
+				int a2 = current.number / 100;
+				int b2 = (current.number % 100) / 10;
+				int c2 = current.number % 10;
+
+				if (a1 == a2) strikeCount++;
+				if (b1 == b2) strikeCount++;
+				if (c1 == c2) strikeCount++;
+
+				if (a1 == b2 || a1 == c2) ballCount++;
+				if (b1 == a2 || b1 == c2) ballCount++;
+				if (c1 == a2 || c1 == b2) ballCount++;
+
+				if (strikeCount == current.strike && ballCount == current.ball) {
+					count++;
+				}
 			}
 
-			if (find(a2, b2, c2)) {
-				count++;
-			}
-		}
-
-		System.out.println(count);
-	}
-
-	private static boolean find(int a2, int b2, int c2) {
-
-		for (int i = 0; i < list.size(); i++) {
-			BallCount curBallCount = list.get(i);
-
-			int a1 = curBallCount.number / 100;
-			int b1 = curBallCount.number / 10 - (a1 * 10);
-			int c1 = curBallCount.number % 10;
-
-			int st1 = curBallCount.strike;
-			int ba1 = curBallCount.ball;
-
-
-			int st2 = 0;
-			int ba2 = 0;
-
-			if (a1 == a2) st2++;
-			if (b1 == b2) st2++;
-			if (c1 == c2) st2++;
-
-			if (a2 == b1 || a2 == c1) ba2++;
-			if (b2 == a1 || b2 == c1) ba2++;
-			if (c2 == a1 || c2 == b1) ba2++;
-
-			if (st1 != st2 || ba1 != ba2) {
-				return false;
+			if (count == N) {
+				answer++;
 			}
 		}
 
-
-		return true;
+		System.out.println(answer);
 	}
 
 }
