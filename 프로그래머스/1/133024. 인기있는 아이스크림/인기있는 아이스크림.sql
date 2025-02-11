09159ada08e6f5d24f2f4871c11fc898a10d5677
JeongSeok <ljs0429777@gmail.com>
2025-02-11 22:26:21 +0900

    [level 1] Title: 인기있는 아이스크림, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
+++ 프로그래머스/1/133024. 인기있는 아이스크림/인기있는 아이스크림.sql
@@ -1,4 +1,7 @@
 -- 코드를 입력하세요
-SELECT FLAVOR
-FROM FIRST_HALF
-ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID ASC;
+-- 조건: 아이스크림의 맛 출력
+-- 정렬: 총주문량을 기준으로 내림차순, 출하 번호로 오름차순
+SELECT
+    flavor
+FROM first_half
+ORDER BY total_order DESC, shipment_id ASC;
